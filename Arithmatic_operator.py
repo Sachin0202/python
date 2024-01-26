296fa55fa2bcc1a00c56c005d63532b44b05ac40
Sachin <143882687+Sachin0202@users.noreply.github.com>
2024-01-26 22:24:46 +0530

    Create Arithmatic_operator.py

---
+++ Arithmatic_operator.py
@@ -0,0 +1,8 @@
+first = input("enter the first num:")
+second = input("enter the second num:")
+sum = "+"
+sub = "-"
+mult = "*"
+operator = input(sum)
+operator = input(sub)
+operator = input(mult)
